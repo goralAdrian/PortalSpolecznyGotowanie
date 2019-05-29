7b05b4b7b917be606785760e5cb07cce40b19738
goralAdrian <37652885+goralAdrian@users.noreply.github.com>
2019-05-29 09:08:12 +0200

    Add files via upload

---
+++ src/components/komentarzePodPostem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Podforum extends React.Component{
+	render(){
+		//const file = require("./text.txt");
+		return(
+		<podforum>
+			<br/>
+			<b>KOMENTARZE:</b>
+			<br/>
+		</podforum>
+		);
+	}
+}
---
+++ src/components/postUzytkownika.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Podforum extends React.Component{
+	render(){
+		//const file = require("./text.txt");
+		return(
+		<podforum>
+			<center>
+				<b>PRZEPIS:</b>
+				<br/>
+				<b>AUTOR:</b>
+				<br/>
+			</center>
+		</podforum>
+		);
+	}
+}
